7b5b3064df8e1faa09ad4893f64e8eddf1e78a56
watch <490772448@qq.com>
2021-12-22 18:14:30 +0800

    try to use another test file

--- src/test_driver/linux-cirque/MobileDeviceTest.py
+++ src/test_driver/linux-cirque/MobileDeviceTest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Copyright (c) 2021 Project CHIP Authors
+Copyright (c) 2020 Project CHIP Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,13 +17,12 @@ limitations under the License.
 
 import logging
 import os
-import pprint
 import time
 import sys
 
 from helper.CHIPTestBase import CHIPVirtualHome
 
-logger = logging.getLogger('MobileDeviceTest')
+logger = logging.getLogger('CHIPEchoTest')
 logger.setLevel(logging.INFO)
 
 sh = logging.StreamHandler()
@@ -32,25 +31,21 @@ sh.setFormatter(
         '%(asctime)s [%(name)s] %(levelname)s %(message)s'))
 logger.addHandler(sh)
 
-CHIP_PORT = 5540
-
-CIRQUE_URL = "http://localhost:5000"
 CHIP_REPO = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "..", "..", "..")
-TEST_EXTPANID = "fedcba9876543210"
 
 DEVICE_CONFIG = {
     'device0': {
-        'type': 'MobileDevice',
+        'type': 'CHIP-Echo-Requester',
         'base_image': 'connectedhomeip/chip-cirque-device-base',
-        'capability': ['Interactive', 'TrafficControl', 'Mount'],
+        'capability': ['Thread', 'Interactive', 'TrafficControl', 'Mount'],
         'rcp_mode': True,
         'docker_network': 'Ipv6',
         'traffic_control': {'latencyMs': 100},
         "mount_pairs": [[CHIP_REPO, CHIP_REPO]],
     },
     'device1': {
-        'type': 'CHIPEndDevice',
+        'type': 'CHIP-Echo-Responder',
         'base_image': 'connectedhomeip/chip-cirque-device-base',
         'capability': ['Thread', 'Interactive', 'TrafficControl', 'Mount'],
         'rcp_mode': True,
@@ -60,67 +55,47 @@ DEVICE_CONFIG = {
     }
 }
 
+CHIP_PORT = 5540
 
-class TestPythonController(CHIPVirtualHome):
+CIRQUE_URL = "http://localhost:5000"
+
+
+class TestEcho(CHIPVirtualHome):
     def __init__(self, device_config):
         super().__init__(CIRQUE_URL, device_config)
         self.logger = logger
 
     def setup(self):
         self.initialize_home()
+        self.connect_to_thread_network()
 
     def test_routine(self):
-        self.run_controller_test()
+        self.run_data_model_test()
 
-    def run_controller_test(self):
-        ethernet_ip = [device['description']['ipv6_addr'] for device in self.non_ap_devices
-                       if device['type'] == 'CHIPEndDevice'][0]
-        server_ids = [device['id'] for device in self.non_ap_devices
-                      if device['type'] == 'CHIPEndDevice']
+    def run_data_model_test(self):
+        resp_ips = [device['description']['ipv6_addr'] for device in self.non_ap_devices
+                    if device['type'] == 'CHIP-Echo-Responder']
+        resp_ids = [device['id'] for device in self.non_ap_devices
+                    if device['type'] == 'CHIP-Echo-Responder']
         req_ids = [device['id'] for device in self.non_ap_devices
-                   if device['type'] == 'MobileDevice']
-
-        for server in server_ids:
-            self.execute_device_cmd(server, "CHIPCirqueDaemon.py -- run {} --thread".format(
-                os.path.join(CHIP_REPO, "out/debug/standalone/chip-all-clusters-app")))
-
-        self.reset_thread_devices(server_ids)
+                   if device['type'] == 'CHIP-Echo-Requester']
 
         req_device_id = req_ids[0]
 
-        self.execute_device_cmd(req_device_id, "pip3 install {}".format(os.path.join(
-            CHIP_REPO, "out/debug/linux_x64_gcc/controller/python/chip-0.0-cp37-abi3-linux_x86_64.whl")))
+        for id in resp_ids:
+            self.execute_device_cmd(id, "CHIPCirqueDaemon.py -- run gdb -batch -return-child-result -q -ex run -ex bt {}".format(
+                os.path.join(CHIP_REPO, "out/debug/linux_x64_gcc/chip-echo-responder")))
 
-        command = "gdb -return-child-result -q -ex run -ex bt --args python3 {} -t 150 -a {}".format(
+        command = "gdb -return-child-result -q -ex run -ex bt --args " + \
             os.path.join(
-                CHIP_REPO, "src/controller/python/test/test_scripts/mobile-device-test.py"),
-            ethernet_ip)
-        ret = self.execute_device_cmd(req_device_id, command)
+                CHIP_REPO, "out/debug/linux_x64_gcc/chip-echo-requester") + " {}"
 
-        self.assertEqual(ret['return_code'], '0',
-                         "Test failed: non-zero return code")
-
-        # Check if the device is in thread network.
-        self.check_device_thread_state(
-            server_ids[0], expected_role=['leader'], timeout=5)
-
-        # Check if the device is attached to the correct thread network.
-        for device_id in server_ids:
-            reply = self.execute_device_cmd(device_id, 'ot-ctl extpanid')
-            self.assertEqual(reply['output'].split()[0].strip(), TEST_EXTPANID)
-
-        # Check if device can be controlled by controller
-        for device_id in server_ids:
-            self.logger.info("checking device log for {}".format(
-                self.get_device_pretty_id(device_id)))
-            self.assertTrue(self.sequenceMatch(self.get_device_log(device_id).decode('utf-8'), [
-                "Received command for Endpoint=1 Cluster=0x0000_0006 Command=0x0000_0001",
-                "Toggle on/off from 0 to 1",
-                "Received command for Endpoint=1 Cluster=0x0000_0006 Command=0x0000_0000",
-                "Toggle on/off from 1 to 0",
-                "No Cluster 0x0000_0006 on Endpoint 0xe9"]),
-                "Datamodel test failed: cannot find matching string from device {}".format(device_id))
+        for ip in resp_ips:
+            ret = self.execute_device_cmd(
+                req_device_id, command.format(ip))
+            self.assertEqual(
+                ret['return_code'], '0', "{} failure: {}".format("Echo", ret['output']))
 
 
 if __name__ == "__main__":
-    sys.exit(TestPythonController(DEVICE_CONFIG).run_test())
+    sys.exit(TestEcho(DEVICE_CONFIG).run_test())
